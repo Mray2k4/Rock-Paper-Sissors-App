3a5200af44b964a47f4094a03ae62bdf095274ff
Marque Ray <mray2k4@yahoo.com>
2023-04-30 10:10:16 -0700

    added user choice and random number

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
-const game = ['Rock', 'Paper', 'Sissors'];
-
+let game = ['Rock', 'Paper', 'Sissors'];
+let games = [];
 
 for (let i = 0; i < game.length; i++) {
     const ele = game[i];
@@ -9,27 +9,67 @@ for (let i = 0; i < game.length; i++) {
     console.log(games);
 }
 */
+
+
 const rock = document.getElementById('item-1');
 const paper = document.getElementById('item-2');
 const sissors = document.getElementById('item-3');
 
-const msg1 = document.getElementById('message1');
-const msg2 = document.getElementById('message2');
-const msg3 = document.getElementById('message3');
+let msg1 = document.getElementById('message1');
+let msg2 = document.getElementById('message2');
+let msg3 = document.getElementById('message3');
 
-let answer = Math.floor(Math.random() * 3) + 1;
+
+let user = 3;
+
 
 
 rock.addEventListener('click', function() {
-    rock.src = "/images/cartoon rock.png"; 
+    msg1.innerHTML = '<b>User picked rock</b>';
+    let answer = Math.floor(Math.random() * 3) + 1;
+    if(user == answer) {
+        msg2.innerHTML = '<b>Computer chose rock</b>';
+        msg3.innerHTML = "<b>It's a TIE!</b>";
+    } else if(user > answer){
+       msg2.innerHTML = '<b>Computer chose sissors</b>';
+       msg3.innerHTML = "<b>User WON!</b>";
+        
+    } else if(user !== answer) {
+       msg2.innerHTML = '<b>Computer chose paper</b>';
+       msg3.innerHTML = "<b>Computer WON!</b>";
+    }
 });
 
 paper.addEventListener('click', function() {
-    paper.src = "/images/cartoon paper.png"; 
+    msg1.innerHTML = '<b>User picked paper</b>';
+    let answer = Math.floor(Math.random() * 3) + 1;
+    if(user == answer) {
+        msg2.innerHTML = '<b>Computer chose paper</b>';
+        msg3.innerHTML = "<b>It's a TIE!</b>";
+    } else if(user > answer){
+       msg2.innerHTML = "<b>Computer chose rock</b>";
+       msg3.innerHTML = "<b>User WON!</b>";
+        
+    } else {
+       msg2.innerHTML = '<b>Computer chose sissors</b>';
+       msg3.innerHTML = "<b>Computer WON!</b>";
+    } 
 });
 
 sissors.addEventListener('click', function() {
-    sissors.src = "/images/cartoon sissors.png"; 
+    msg1.innerHTML = '<b>User picked sissors</b>';
+    let answer = Math.floor(Math.random() * 3) + 1;
+    if(user == answer) {
+        msg2.innerHTML = '<b>Computer chose sissors</b>';
+        msg3.innerHTML = "<b>It's a TIE!</b>";
+    } else if(user > answer){
+        msg2.innerHTML = '<b>Computer chose sissors</b>';
+        msg3.innerHTML = "<b>User WON!</b>";
+        
+    } else {
+       msg2.innerHTML = '<b>Computer chose rock</b>';
+       msg3.innerHTML = "<b>Computer WON!</b>";
+    } 
 });
     
     
