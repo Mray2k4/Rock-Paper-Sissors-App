7d44ba2658e15f998b88e9bb22d27fd09ceedc7d
Marque Ray <mray2k4@yahoo.com>
2023-05-01 11:04:46 -0700

    added score when anyone wins

--- script.js
+++ script.js
@@ -15,26 +15,33 @@ const rock = document.getElementById('item-1');
 const paper = document.getElementById('item-2');
 const sissors = document.getElementById('item-3');
 
+
 let msg1 = document.getElementById('message1');
 let msg2 = document.getElementById('message2');
 let msg3 = document.getElementById('message3');
 
+let userChoice = 2;
+let userScore = 0; 
+let compScore = 0;
 
-let user = 2;
-
-
+const scoreUser = document.getElementById('userScore');
+const scoreComp = document.getElementById('compScore')
+let score = 0;
 
 rock.addEventListener('click', function() {
     msg1.innerHTML = '<b>User picked rock</b>';
     let answer = Math.floor(Math.random() * 3) + 1;
-    if(user == answer) {
+    if(userChoice == answer) {
         msg2.innerHTML = '<b>Computer chose rock</b>';
         msg3.innerHTML = "<b>It's a TIE!</b>";
-    } else if(user > answer){
+    } else if(userChoice > answer){
+        score += 1;
+       scoreUser.innerHTML = score;
        msg2.innerHTML = '<b>Computer chose sissors</b>';
        msg3.innerHTML = "<b>User WON!</b>";
-        
     } else {
+       score += 1;
+       scoreComp.innerHTML = score;
        msg2.innerHTML = '<b>Computer chose paper</b>';
        msg3.innerHTML = "<b>Computer WON!</b>";
     }
@@ -43,14 +50,17 @@ rock.addEventListener('click', function() {
 paper.addEventListener('click', function() {
     msg1.innerHTML = '<b>User picked paper</b>';
     let answer = Math.floor(Math.random() * 3) + 1;
-    if(user == answer) {
+    if(userChoice == answer) {
         msg2.innerHTML = '<b>Computer chose paper</b>';
         msg3.innerHTML = "<b>It's a TIE!</b>";
-    } else if(user > answer){
+    } else if(userChoice > answer){
+        score += 1;
+       scoreUser.innerHTML = score;
        msg2.innerHTML = "<b>Computer chose rock</b>";
        msg3.innerHTML = "<b>User WON!</b>";
-        
     } else {
+       score += 1;
+       scoreComp.innerHTML = score;
        msg2.innerHTML = '<b>Computer chose sissors</b>';
        msg3.innerHTML = "<b>Computer WON!</b>";
     } 
@@ -59,19 +69,24 @@ paper.addEventListener('click', function() {
 sissors.addEventListener('click', function() {
     msg1.innerHTML = '<b>User picked sissors</b>';
     let answer = Math.floor(Math.random() * 3) + 1;
-    if(user == answer) {
+    if(userChoice == answer) {
         msg2.innerHTML = '<b>Computer chose sissors</b>';
         msg3.innerHTML = "<b>It's a TIE!</b>";
-    } else if(user > answer){
+    } else if(userChoice > answer){
+        score += 1;
+        scoreUser.innerHTML = score;
         msg2.innerHTML = '<b>Computer chose paper</b>';
         msg3.innerHTML = "<b>User WON!</b>";
-        
     } else {
-       msg2.innerHTML = '<b>Computer chose rock</b>';
-       msg3.innerHTML = "<b>Computer WON!</b>";
-    } 
+        score += 1;
+        scoreComp.innerHTML = score;
+        msg2.innerHTML = '<b>Computer chose rock</b>';
+        msg3.innerHTML = "<b>Computer WON!</b>";
+    }
+
+
 });
-    
-    
+
+
 
 
